cf5b134d627cef5d74529085bbcb2fccdba3fa33
valen-web <128190072+valen-web@users.noreply.github.com>
2024-02-28 15:03:56 -0500

    Add files via upload

--- public-client/client.js
+++ public-client/client.js
@@ -1,1 +1,31 @@
-const socket = io();
+
+const users = [];
+
+function addUser(username, email) {
+    const id = users.length + 1;
+
+    const newUser = {
+        id,
+        username,
+        email,
+    };
+
+    users.push(newUser);
+
+    return newUser;
+}
+
+function getAllUsers() {
+    return users;
+}
+
+function findUserByUsername(username) {
+    return users.find(user => user.username === username);
+}
+
+
+module.exports = {
+    addUser,
+    getAllUsers,
+    findUserByUsername
+};
